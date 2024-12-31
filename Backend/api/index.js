e80ffddc706c5af7f6235aff08b0135a45fb843f
Ritvik Arora <ritvik.arora15.ra@gmail.com>
2024-12-31 21:47:39 +0530

    backend update

--- Backend/api/index.js
+++ Backend/api/index.js
@@ -3,21 +3,26 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 
-const { userRouter } = require("./Routes/user");
-const { gameRouter } = require("./Routes/game");
-
+const { userRouter } = require("../Routes/user");
+const { gameRouter } = require("../Routes/game");
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.get("/", (req, res) => {
+  res.send("Hello from Express on Vercel!");
+});
 
 app.use("/api/v1/user", userRouter);
 app.use("/api/v1/game", gameRouter);
 
+module.exports = app;
 async function main() {
   await mongoose.connect(process.env.MONGO_URL);
-  app.listen(3001);
-  console.log("listening on port 3001");
+  if (require.main === module) {
+    const PORT = process.env.PORT || 3000;
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+  }
 }
 main();
